c10532bedd5e2cc5d089bf92bd6d5515cd01ce4a
AdoyoClifford <adoyoclifford@gmail.com>
2023-03-04 13:53:01 +0300

    initial commit

--- app/src/main/java/com/adoyo/dictionaryapp/presentation/HomeScreen.kt
+++ app/src/main/java/com/adoyo/dictionaryapp/presentation/HomeScreen.kt
@@ -5,7 +5,10 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.*
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -13,8 +16,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,7 +25,6 @@ import com.adoyo.dictionaryapp.data.remote.Meaning
 import com.adoyo.dictionaryapp.presentation.uistate.DefinitionUiState
 import com.adoyo.dictionaryapp.presentation.uistate.components.SearchField
 import com.adoyo.dictionaryapp.ui.theme.DictionaryAppTheme
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 
 @Composable
