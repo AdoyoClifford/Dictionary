ddc2801b6f8b0f83345c5a19a47b089cc42ff6d5
AdoyoClifford <adoyoclifford@gmail.com>
2023-03-11 06:23:13 +0300

    initial commit

--- app/src/main/java/com/adoyo/dictionaryapp/presentation/uistate/components/PartsOfSpeech.kt
+++ app/src/main/java/com/adoyo/dictionaryapp/presentation/uistate/components/PartsOfSpeech.kt
@@ -5,14 +5,13 @@ import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
